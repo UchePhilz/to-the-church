75c1b9c703a50701ed3afa676bdd6b72bc994ad4
Uchephilz <uchephilz@gmail.com>
2026-03-16 06:36:16 +0100

    Refine homepage content for grammar and readability improvements

--- views/site/index.php
+++ views/site/index.php
@@ -20,17 +20,17 @@ $this->params['breadcrumbs'][] = "";
             <div class="card">
                 <div class="card-body">
                     <div class="card-text" style="font-size: x-large;">
-                        God has been very gracious to the world and to the church. <br>
-                        He has given us His Spirit so we can be his able children in this world.<br>
+                        God has been very gracious to the world and to the Church.<br>
+                        He has given us His Spirit so that we may be His able children in this world.<br>
                         <hr>
                         These writings are for:
                         <ul>
-                            <li>Those that are seeking God in their heart.</li>
-                            <li>Those that say they believe but are lost.</li>
-                            <li>Those that are now confident.</li>
+                            <li>Those who are seeking God in their heart.</li>
+                            <li>Those who say they believe, yet are lost.</li>
+                            <li>Those who are now confident.</li>
                         </ul>
                         <hr>
-                        The Spirit of God will Guide you to the truth, and you shall see yourself as you see Him<br>
+                        The Spirit of God will guide you to the truth, and you shall see yourself as you see Him.<br>
                     </div>
                 </div>
                 <?= Html::a('View Writings', ['site/writings'], ['class' => 'btn btn-outline-secondary mt-2']) ?>
